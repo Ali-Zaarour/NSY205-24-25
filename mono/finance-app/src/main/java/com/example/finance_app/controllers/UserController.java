88d4385a986e15e0152730e879268a3f18154d11
Ali Zaarour <zaarour.a@outlook.com>
2025-01-07 00:39:15 +0200

    upd config

--- mono/finance-app/src/main/java/com/example/finance_app/controllers/UserController.java
+++ mono/finance-app/src/main/java/com/example/finance_app/controllers/UserController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Tag(name = "User api")
-@RequestMapping("/admin/users")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
