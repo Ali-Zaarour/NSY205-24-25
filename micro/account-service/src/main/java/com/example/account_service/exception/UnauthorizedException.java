20909cd37716b0053adefe81dbfc1a952af2925c
Ali Zaarour <zaarour.a@outlook.com>
2025-01-12 00:09:42 +0200

    feat: inti account service with docker and kubernetes configuration

---
+++ micro/account-service/src/main/java/com/example/account_service/exception/UnauthorizedException.java
@@ -0,0 +1,22 @@
+package com.example.account_service.exception;
+
+
+import com.example.account_service.exception.config.ExceptionMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends AuthenticationException {
+    public UnauthorizedException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public UnauthorizedException() {
+        super(ExceptionMessage.UNAUTHORIZED_ACTION);
+    }
+
+    public UnauthorizedException(String msg) {
+        super(ExceptionMessage.UNAUTHORIZED_ACTION + " " + msg);
+    }
+}
